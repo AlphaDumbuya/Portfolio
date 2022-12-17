7fa2c9496d1fe402e766ef108fbff84ac63b7217
Your Name <you@example.com>
2022-12-17 16:11:48 +0000

    Changes made

---
+++ css/app.js
@@ -0,0 +1,16 @@
+"use strict"
+const projectOneBtn = document.querySelector("button-p1btn")
+const projectTwoBtn = document.querySelector("button-p1btn")
+const projectThreeBtn = document.querySelector("button-p1btn")
+
+projectOneBtn.addEventListener("click",() =>{
+    alert("This project is under development, please check in some other time ")
+    });
+
+    projectTwoBtn.addEventListener("click",() =>{
+        alert("This project is under development, please check in some other time ")
+        });
+
+        projectThreeBtn.addEventListener("click",() =>{
+            alert("This project is under development, please check in some other time ")
+            });
