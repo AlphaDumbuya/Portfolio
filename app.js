01223b28e636addb3cec6229f1fc644631d700dd
Your Name <you@example.com>
2022-12-17 16:31:47 +0000

    project code correction

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const projectTwoBtn = document.querySelector(".p2btn")
 const projectThreeBtn = document.querySelector(".p3btn")
 
 projectOneBtn.addEventListener("click",() =>{
-    alert("This project is under development, please check in some other time ")
+    alert("This portfolio app is under development, please check in some other time ")
     });
 
     projectTwoBtn.addEventListener("click",() =>{
-        alert("This project is under development, please check in some other time ")
+        alert("This meal app is under development, please check in some other time ")
         });
 
         projectThreeBtn.addEventListener("click",() =>{
-            alert("This project is under development, please check in some other time ")
+            alert("This recipe is under development, please check in some other time ")
             });
